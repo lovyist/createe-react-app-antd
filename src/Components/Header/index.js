c670c643cc1bd55e2bfa1de68f70f46ef4baed39
mhh <lovyist@163.com>
2017-03-29 18:32:38 +0800

    first commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by lovyist on 2017/3/29.
+ */
+import React, { Component } from 'react';
+import { browserHistory, Link } from 'react-router';
+const Header = ({title, hasGoBack}) => (
+    <div className="nav-top-p">
+        {
+            hasGoBack &&
+            <div className="icon-back" onClick={() => {
+        browserHistory.goBack()
+      }}>
+                <span></span>
+                <p className="goBack">返回</p>
+            </div>
+        }
+        <div className="nav-top-title"><span className="title-mi">米饭&nbsp;-&nbsp;</span><span
+            className="opera-title">{title}</span></div>
+        <div className="icon-home"><Link to="/"><span></span></Link></div>
+    </div>
+)
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    hasGoBack: PropTypes.bool
+}
+
+Header.defaultProps = {
+    hasGoBack: true
+}
+
+export default Header;
